04c2e6642dfc399b28d62b05569d5922bee93c91
asimkhan8107 <110652576+asimkhan8107@users.noreply.github.com>
2023-07-22 22:09:27 +0530

    Add files via upload

---
+++ Day-18/boothMultiplier.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.07.2023 21:52:19
+// Design Name: 
+// Module Name: boothMultiplier
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module boothMultiplier(PRODUCT, A, B);
+  output reg signed [7:0] PRODUCT;
+  input signed [3:0] A, B;
+
+  reg [1:0] temp;
+  integer i;
+  reg e;
+  reg [3:0] B1;
+
+  always @(A,B)
+  begin
+    PRODUCT = 8'd0;
+    e = 1'b0;
+    B1 = -B;
+    
+    for (i=0; i<4; i=i+1)
+    begin
+      temp = { A[i], e };
+      case(temp)
+        2'd2 : PRODUCT[7:4] = PRODUCT[7:4] + B1;
+        2'd1 : PRODUCT[7:4] = PRODUCT[7:4] + B;
+      endcase
+      PRODUCT = PRODUCT >> 1;
+      PRODUCT[7] = PRODUCT[6];
+      e=A[i];
+      
+    end
+  end
+  
+endmodule
